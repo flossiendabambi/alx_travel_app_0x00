0f13f0b9e66b1db16976874ad1e02ae0833bd1bc
Florence Ndabambi <flossiendabambi@gmail.com>
2025-05-30 12:53:35 +0200

    Views.py

--- alx_travel_app/listings/urls.py
+++ alx_travel_app/listings/urls.py
@@ -1,6 +1,10 @@
-from django.urls import path
-from . import views
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import ListingViewSet
+
+router = DefaultRouter()
+router.register(r'listings', ListingViewSet, basename='listing')
 
 urlpatterns = [
-    path('', views.home, name="home"),
+    path('', include(router.urls)),
 ]
--- alx_travel_app/listings/views.py
+++ alx_travel_app/listings/views.py
@@ -1,6 +1,26 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import User, Listing, Booking, Review
+from .serializers import ListingSerializer, BookingSerializer
 from django.http import JsonResponse
-# Create your views here.
 
-def home(request):
-    return JsonResponse({"message": "Welcome to the Listings API"})
+
+# Create your views here.
+class ListingViewSet(viewsets.ModelViewSet):
+    queryset = Listing.objects.all()
+    serializer_class = ListingSerializer
+    
+    
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        name = self.request.query_params('name')
+        description = self.request.query_params('description')
+        location = self.request.query_params('location')
+        
+        if name:
+            queryset = queryset.filter(genre__iexact=name)
+        if description:
+            queryset = queryset.filter(genre__iexact=description)
+        return queryset
+            
+        
